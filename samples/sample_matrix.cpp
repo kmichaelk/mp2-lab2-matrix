8f7818d1ffc2ae2f5b6b84a1addbbbb832b15729
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2023-10-18 00:03:20 +0300

    Update sample

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -8,13 +8,15 @@
 #include "tmatrix.h"
 //---------------------------------------------------------------------------
 
-void main()
+using namespace std;
+
+int main()
 {
   TDynamicMatrix<int> a(5), b(5), c(5);
   int i, j;
 
   setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
+  cout << "Тестирование класса работы с матрицами"
     << endl;
   for (i = 0; i < 5; i++)
     for (j = i; j < 5; j++ )
@@ -26,5 +28,7 @@ void main()
   cout << "Matrix a = " << endl << a << endl;
   cout << "Matrix b = " << endl << b << endl;
   cout << "Matrix c = a + b" << endl << c << endl;
+
+  return 0;
 }
 //---------------------------------------------------------------------------
